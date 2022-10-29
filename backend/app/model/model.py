6f63339047845aa861dec0a576fd6c7b0a31ff6c
kozaczinioo <jakubprusakiewicz@gmail.com>
2022-10-29 12:48:56 +0200

    feat: add mock backend

---
+++ backend/app/model/model.py
@@ -0,0 +1,22 @@
+from random import choice
+
+from fastapi import APIRouter, UploadFile
+from starlette.responses import JSONResponse
+
+from ..config import settings
+from ..logger import setup_custom_logger
+
+FIRST_MODULE_TAG = "MODEL"
+router = APIRouter()
+logger = setup_custom_logger('model', settings.LOG_LEVEL)
+
+example_moves = ['Nf3', 'Nf6', 'c4', 'g6', 'Nc3', 'Bg7', 'd4', 'Bf4', 'Qb3']
+
+
+@router.post("/predict", tags=[FIRST_MODULE_TAG])
+async def predict(file: UploadFile):
+    logger.info(f"predict with file {file.filename}")
+    return JSONResponse(
+        status_code=200,
+        content={"call": choice(example_moves)}
+    )
