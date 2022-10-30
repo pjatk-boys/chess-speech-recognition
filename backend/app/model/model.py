8d48b8a6dee344174c1a515cf23868e285a8bd16
kozaczinioo <jakubprusakiewicz@gmail.com>
2022-10-30 14:20:14 +0100

    fix: add default file

--- backend/app/model/model.py
+++ backend/app/model/model.py
@@ -1,6 +1,6 @@
 from random import choice
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, File
 from starlette.responses import JSONResponse
 
 from ..config import settings
@@ -14,7 +14,7 @@ example_moves = ['Nf3', 'Nf6', 'c4', 'g6', 'Nc3', 'Bg7', 'd4', 'Bf4', 'Qb3']
 
 
 @router.post("/predict", tags=[FIRST_MODULE_TAG])
-async def predict(file: UploadFile):
+async def predict(file: UploadFile = File(...)):
     logger.info(f"predict with file {file.filename}")
     return JSONResponse(
         status_code=200,
